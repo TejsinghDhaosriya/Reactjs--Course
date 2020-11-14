cc8839b4ba78592af002f00bb50400a6f02536af
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-11-14 22:46:19 +0530

    ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render('kya dikhana hai ','kaha dikhanaa','callback fun');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ ReactDOM.render(
+    <React.Fragment>
+ <h1>Hello world</h1>
+  <h5>Reactjs course</h5>
+ </React.Fragment>
+ ,document.getElementById('root'));
+
+//create element pure js 
+//same thing which is done by render method
+
+// const h1 = document.createElement('h1');
+// h1.innerHTML = "Reactjs course"
+// document.getElementById("root").appendChild(h1);
+
+
+//from react 16 onward it is possible to return array of element in render method
+
+
+// ReactDOM.render(
+// [
+// <h1>H2ello world</h1>,
+// <h5>Reactjs course</h5>
+// ]
+// ,document.getElementById('root'));
